e06c3a476c385c2d8218544f6f65a18c22799366
Amit Barman <ab2762314@gmail.com>
2026-01-08 17:10:12 +0530

    first  commit

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+import mongoose, { connections } from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    _id:{type:String, require: true},
+    email:{type:String, require: true},
+    full_name:{type:String, require: true},
+    username:{type:String, unique: true},
+    bio:{type:String, default: 'Hey there! I m using Spyner'},
+    profile_picture:{type:String, default: ''},
+    cover_photo:{type:String, default:''},
+    location:{type:String, default:''},  
+    followers:[{type:String, ref:'User'}],
+    following:[{type:String, ref:'User'}],
+    connections:[{type:String, ref:'User'}],  
+},{timestamps: true, minimize: false})
+
+
+const User =mongoose.model('User',userSchema)
+
+export default User
